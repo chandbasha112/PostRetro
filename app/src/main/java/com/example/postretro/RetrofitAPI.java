223561ab0e5ffc5812fec75074fbeb1eb167163f
Riyaz <chandbasha0501@gmail.com>
2021-11-19 16:13:12 +0530

    succesfully dine

---
+++ app/src/main/java/com/example/postretro/RetrofitAPI.java
@@ -0,0 +1,13 @@
+package com.example.postretro;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+
+public interface RetrofitAPI {
+
+
+    @POST("users")    //on below line we are creating a method to post our data.
+    Call<DataModal> createPost(@Body DataModal dataModal);
+}
